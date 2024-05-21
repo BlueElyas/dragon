ceffde0cfc184473eeb34c69599895d2acaa2f4d
Jo Pickering <joanna.r.pickering@gmail.com>
2024-05-21 11:30:23 +0100

    CSS for ResultsCard

--- src/components/MainContent/AskPage/ResultsFeature/ResultsCard.jsx
+++ src/components/MainContent/AskPage/ResultsFeature/ResultsCard.jsx
@@ -1,8 +1,9 @@
 import React from "react"
+import styles from "ResultsCard.module.css"
 
 function ResultsCard({ title, description }) {
   return (
-    <div>
+    <div className={styles.card}>
       <h2>{title}</h2>
       <p>{description}</p>
     </div>
