5dfdfe176bf9e40b1f4b6565816c199655c6dd18
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-05-21 11:47:16 +0100

    Remove useeffect unused var in sitenav.jsx

--- src/components/SiteHeader/SiteNav/SiteNav.jsx
+++ src/components/SiteHeader/SiteNav/SiteNav.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import styles from "./SiteNav.module.css"
 import { NavLink } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
