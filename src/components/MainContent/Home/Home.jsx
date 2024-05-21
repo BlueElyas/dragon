6a0be358c86824d0e248a41abe0035cd3c6dd829
Jo Pickering <joanna.r.pickering@gmail.com>
2024-05-21 09:50:54 +0100

    Button CSS

--- src/components/MainContent/Home/Home.jsx
+++ src/components/MainContent/Home/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
     <div>
       <p>This is a homepage</p>
       <NavLink to="about">about</NavLink>
+      <button className="ButtonPurple">For styling</button>
       <img
         style={{ width: "100px", display: "block" }}
         src="assets/dragon-napoleon-dynamite-instructor.png"
