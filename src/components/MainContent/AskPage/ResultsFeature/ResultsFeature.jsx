81a0a2042881adfe317af08757cb19316c68ff3d
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-05-21 16:04:17 +0100

    Add hasError and isLoading conditionals to resultsfeature.jsx

--- src/components/MainContent/AskPage/ResultsFeature/ResultsFeature.jsx
+++ src/components/MainContent/AskPage/ResultsFeature/ResultsFeature.jsx
@@ -1,9 +1,33 @@
 import React from "react"
 import ResultsCard from "./ResultsCard"
 import { useSelector } from "react-redux"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDragon } from "@fortawesome/free-solid-svg-icons"
 
 function ResultsComponent() {
   const results = useSelector((state) => state.question.results)
+  const isLoading = useSelector((state) => state.question.isLoading)
+  const hasError = useSelector((state) => state.question.hasError)
+
+  if (isLoading) {
+    return <FontAwesomeIcon icon={faDragon} className="fa-2xl" />
+  }
+
+  if (hasError) {
+    return (
+      <>
+        <div>
+          <h4>No Arkenstone for you.</h4>
+          <p>Ask a new question, fool.</p>
+          <img
+            src="assets/dragon-napoleon-dynamite-instructor.png"
+            alt=""
+            style={{ width: "100px" }}
+          />
+        </div>
+      </>
+    )
+  }
 
   if (results.results) {
     return (
