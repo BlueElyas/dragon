4d9d68f1b9b2609114a32fdb11a77de03bfe9814
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-05-21 11:48:03 +0100

    Add alt tag to logo

--- src/components/SiteHeader/Logo/Logo.jsx
+++ src/components/SiteHeader/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import styles from "./Logo.module.css"
 function Logo() {
   return (
     <div className={styles.logo}>
-      <img src="assets/dragon-logo.png" alt="" />
+      <img src="assets/dragon-logo.png" alt="cartoonish dragon logo" />
       <h3>Ask DargonAI </h3>
     </div>
   )
