1137e0ef25c138a10ba1a30764efd4f39c91559b
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-23 10:52:58 +0100

    Add asyncthunk api call to products page, allowing them to render sans the images

--- src/components/MainContent/Products/Products.jsx
+++ src/components/MainContent/Products/Products.jsx
@@ -1,23 +1,68 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./Products.module.css"
 import ProductCard from "./ProductCard/ProductCard"
 import ProductsSearch from "./ProductsSearch/ProductsSearch"
+import { fetchExperiences } from "./productsSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDragon } from "@fortawesome/free-solid-svg-icons"
+import { setExperiences } from "./productsSlice"
+
 function Products() {
   const [input, setInput] = useState("")
+  const dispatch = useDispatch()
+  const selector = useSelector((state) => state.dragonStore.dragonExperiences)
+  const isLoading = useSelector((state) => state.dragonStore.isLoading)
+  const hasError = useSelector((state) => state.dragonStore.hasError)
+  console.log(isLoading)
+  console.log(hasError)
+  console.log(selector)
+
+  useEffect(() => {
+    dispatch(fetchExperiences())
+  }, [dispatch])
+  console.log(selector)
+  if (isLoading) {
+    return <FontAwesomeIcon icon={faDragon} className="fa-2xl" />
+  }
+
+  if (hasError) {
+    return (
+      <>
+        <div>
+          <h4>No Arkenstone for you.</h4>
+          <p>Ask a new question, fool.</p>
+          <img
+            src="assets/dragon-napoleon-dynamite-instructor.png"
+            alt=""
+            style={{ width: "100px" }}
+          />
+        </div>
+      </>
+    )
+  }
 
   return (
     <main className={styles.productsContainer}>
       <section>
-        <ProductsSearch/>
+        <ProductsSearch />
       </section>
       <section style={styles.productsFound}>
         <p>6 products found</p>
       </section>
       <section className={styles.productsList}>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {selector.map((experience) => {
+          return (
+            <ProductCard
+              key={experience.id}
+              title={experience.title}
+              desc={experience.description}
+              rating={experience.title}
+              price={experience.price}
+              image={experience.image}
+            />
+          )
+        })}
       </section>
     </main>
   )
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { questionFeatureSlice } from "./components/MainContent/AskPage/QuestionFeature/questionFeatureSlice"
+import { productSlice } from "./components/MainContent/Products/productsSlice"
 
 export const store = configureStore({
   reducer: {
     question: questionFeatureSlice.reducer,
+    dragonStore: productSlice.reducer,
   },
 })
