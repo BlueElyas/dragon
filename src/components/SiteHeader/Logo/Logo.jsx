dddc70958f321b628c9514270aec80fb0fdb3fea
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-05-23 14:06:31 +0100

    Add link to logo.jsx to route to homepage

--- src/components/SiteHeader/Logo/Logo.jsx
+++ src/components/SiteHeader/Logo/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import styles from "./Logo.module.css"
+import { NavLink } from "react-router-dom"
 
 function Logo() {
   return (
     <div className={styles.logo}>
-      <img src="assets/dragon-logo.png" alt="cartoonish dragon logo" />
-      <h3>Ask DargonAI </h3>
+      <a href="/">
+        <img src="assets/dragon-logo.png" alt="cartoonish dragon logo" />
+        <h3>Ask DargonAI </h3>
+      </a>
     </div>
   )
 }
