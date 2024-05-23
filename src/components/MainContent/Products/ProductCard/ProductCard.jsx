ea19f85affa669fd3d2e1b4a550fa15ba1c2905d
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-23 11:41:30 +0100

    add star rating to products page

--- src/components/MainContent/Products/ProductCard/ProductCard.jsx
+++ src/components/MainContent/Products/ProductCard/ProductCard.jsx
@@ -1,13 +1,27 @@
-import React from "react"
+import { useState } from "react"
 import styles from "./ProductCard.module.css"
-
-function ProductCard({ image, title, desc, rating, price }) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar } from "@fortawesome/free-solid-svg-icons"
+import { faStar as faRegular } from "@fortawesome/free-regular-svg-icons"
+function ProductCard({ image, title, desc, stars, price }) {
+  function setRatingCompiler(stars) {
+    const emptyStars = 5 - stars
+    const starsArray = []
+        for(let i=0; i<stars;i++){
+          starsArray.push(<FontAwesomeIcon icon={faStar}/>)
+        }
+        for(let i=0; i<emptyStars;i++){
+          starsArray.push(<FontAwesomeIcon icon={faRegular}/>)
+        }
+        return starsArray.map((star)=> star)
+        }
+  
   return (
     <article className={styles.card}>
       <img src={image} alt="" className={styles.img} />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.desc}>{desc}</p>
-      <p className={styles.rating}>{rating}</p>
+      <p className={styles.stars}>{setRatingCompiler(stars)}</p>
       <p className={styles.price}>£{price}</p>
     </article>
   )
