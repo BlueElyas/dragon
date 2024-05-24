89a37e6e2fd0dd27e272a9ebfe33fdaf587dba6c
Jo Pickering <joanna.r.pickering@gmail.com>
2024-05-24 13:18:57 +0100

    remove use state from prod card

--- src/components/MainContent/ProductsPage/ProductCard/ProductCard.jsx
+++ src/components/MainContent/ProductsPage/ProductCard/ProductCard.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import styles from "./ProductCard.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
